e9b37a0db419af9823a0ac0ba9f0b3681e95b336
Dzmitry Martavoi <Dzmitry Martavoi>
2016-04-12 21:39:48 +0300

    kudu deployment grunt copy glob workaround fix2

--- src/home/deploy.cmd
+++ src/home/deploy.cmd
@@ -105,7 +105,7 @@ IF EXIST "package.json" (
 echo Running grunt
 IF EXIST "gruntfile.js" (
   pushd ./node_modules/grunt
-  eval $NPM_CMD install glob@^6.0.4 --save
+  call :ExecuteCmd npm install glob@^6.0.4 --save
   popd
 
   call .\node_modules\.bin\grunt
